b70661714e1f3c8ef0de7904247329e6d1d9c2b3
Alan Massaro <alan.massar@gmail.com>
2021-01-14 11:42:33 +0100

    create media queries in reset.css

--- wp-content/themes/Malabastheme/archive-restaurant.php
+++ wp-content/themes/Malabastheme/archive-restaurant.php
@@ -1,19 +1,3 @@
 <?php get_header(); ?>
-
-<h1 class="site__heading"><?php post_type_archive_title(); ?></h1>
-
-<main class="site__portfolio">
-	<?php if( have_posts() ) : while( have_posts() ) : the_post(); ?>
-    	<div class="project">
-        	<h2 class="project__title">
-                <a href="<?php the_permalink(); ?>">
-                	<?php the_title(); ?>
-                </a>
-            </h2>
-         	<?php the_post_thumbnail(); ?>
-    	</div>
-    <?php endwhile; endif; ?>
-</main> 
-
-<?php the_posts_pagination(); ?>
+<h1>Hello archive restaurant</h1>
 <?php get_footer(); ?>
