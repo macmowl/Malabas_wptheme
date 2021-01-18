a55c1114872204a5395eb53c67cff6c988b76593
Maud-Pixel <leleuxmaud@gmail.com>
2021-01-18 18:08:19 +0100

    commit form

--- wp-content/themes/Malabastheme/single-restaurants.php
+++ wp-content/themes/Malabastheme/single-restaurants.php
@@ -5,12 +5,16 @@
                 $image_id = get_field( 'banneer_image' );
             
         ?>
-        <section class="banneer_image" style="background-image: url('<?= $image_id['url']?>');">
+        <section class="banneer">
+        <div class="banneer_image" style="background-image: url('<?= $image_id['url']?>');">
+        
             <div class="hero__wrapper">
                 <h2 class="hero__wrapper__subtitle"><?php the_field("subtitle_restaurant");?></h2>
                 <h1 class="hero__wrapper__title"><?php the_field("title_restaurant"); ?></h1>
                 <p><?php the_field("city_restaurant")?></p>
             </div>
+        
+        </div>
         </section>
         <div class="presentation">
                 <div class="presentation__intro">
@@ -76,14 +80,65 @@
                 
         ?>
         </div>
-            <div class="acf-map" data-zoom="<?php echo $zoom; ?>">
-                <div class="marker" data-lat="<?php echo $lat; ?>" data-lng="<?php echo $lng; ?>">
-                </div>
+        <div class="acf-map" data-zoom="<?php echo $zoom; ?>">
+            <div class="marker" data-lat="<?php echo $lat; ?>" data-lng="<?php echo $lng; ?>">
             </div>
+        </div>
 
         <?php endif; ?>
-                            
-        
+        <div class="book">
+            <div class="book__intro">
+                <p class="book__intro__submit">Submit Information to Place Order</p>
+                <p class="book__intro__reserve">RESERVE A TABLE</p>
+            </div>   
+            <div class="reserve">
+                <form>
+                    <div class="reserve__line__1">
+                        <div>
+                            <label for="name">Nom</label>
+                            <input type="text" name="name">
+                        </div>
+                        <div>
+                            <label for="email">Email</label>
+                            <input type="text" name="email">
+                        </div>
+                    </div>
+                    <div class="reserve__line__2">
+                        <div>
+                            <label for="phone_number">Téléphone</label>
+                            <input type="text" name="phone_number">
+                        </div>
+                        <div>
+                            <label for="table">Table pour</label>
+                            <select name="table">
+                                <option value="volvo">1</option>
+                                <option value="volvo">2</option>
+                                <option value="volvo">3</option>
+                                <option value="volvo">4</option>
+                                <option value="volvo">5</option>
+                                <option value="volvo">6</option>
+                                <option value="volvo">7</option>
+                                <option value="volvo">8</option>
+                                <option value="volvo">9</option>
+                                <option value="volvo">10</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div class="reserve__line__3">
+                        <label for="customer_address">Adresse</label>
+                        <input type="text" name="customer_address">
+                    </div>
+                    <div class="reserve__line__4">
+                        <input type="date" id="start" name="trip-start"
+                                value="2018-07-22"
+                                min="2018-01-01" max="2018-12-31">
+                    </div>
+                    <div class="reserve__line__5">
+                        <input type="time">
+                    </div>
+                </form>
+            </div>            
+        </div>
 
 
     
