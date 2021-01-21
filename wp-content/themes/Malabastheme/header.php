e4054c91eb99a51dbababfe59110e8134c8da0d6
Alan Massaro <alan.massar@gmail.com>
2021-01-21 11:10:57 +0100

    mobile recipes done

--- wp-content/themes/Malabastheme/header.php
+++ wp-content/themes/Malabastheme/header.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="<?php bloginfo("charset");?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
-    <title>Header</title>
+    <title><?php wp_title('', true, '' ); ?></title>
     <?php wp_head() ?>
 </head>
 <body <?php body_class(); ?>>
